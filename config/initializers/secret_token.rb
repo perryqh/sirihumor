dd96e3290c62af2fdd8b3ff80fede89d9c227d17
Perry Hertler <perry@hertler.org>
2011-10-20 16:45:44 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sirihumor::Application.config.secret_token = 'c73684a52b85d36dc25a6207627161ab1e47a6c36b172b483321041af363a8569c828a7f429fa1a443c6ad0c195e08dd82faedb485e8529811afa010f21feda1'
