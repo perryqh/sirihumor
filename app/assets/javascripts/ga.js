531a8aa74ac1f8f01e6add96511bfa6c363427a0
Perry Hertler <perry@hertler.org>
2011-10-21 21:34:15 -0500

    adding google analytics

---
+++ app/assets/javascripts/ga.js
@@ -0,0 +1,12 @@
+jQuery(document).ready(function($) {
+  var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+
+  jQuery.getScript(gaJsHost + "google-analytics.com/ga.js", function(){
+    _gaq.push(['_setAccount', 'UA-26482258-1']);
+    _gaq.push(['_trackPageview']);
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  });
+});
+
