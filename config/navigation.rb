b9cf77be465069d688663406e3463bb4ebf31588
Perry Hertler <perry@hertler.org>
2011-10-26 16:45:38 -0500

    everyone needs an icon

--- config/navigation.rb
+++ config/navigation.rb
@@ -2,6 +2,8 @@ SimpleNavigation::Configuration.run do |navigation|
   navigation.selected_class = 'active'
 
   navigation.items do |primary|
+    primary.dom_class = 'pills'
+    primary.item :home, 'Home', root_url
     primary.item :new_post, 'Submit Your Own', new_post_url
     primary.item :faq, 'FAQ', page_path('faq')
   end
